2d7aeaadcabfd8eccc762c673c709d2209a2f60d
yangxiaodong <yangxiaodong@qianmi.com>
2018-07-05 13:54:23 +0800

    add: 添加参数子项规则 ;;

--- example/src/test.ts
+++ example/src/test.ts
@@ -1,19 +1,14 @@
 /**
  * @desc
+ * 搜索引擎参数构造助手 使用演示;;
  *
- * @使用场景
- *
- * @company qianmi.com
- * @Date    2018/6/19
  **/
 // import { esClient } from './es-client';
+
+let a = esClient`/search/123123123?q=sdfsdf&cats=223123&basic=sdfsdfwer&ex_q_属性名=bterms(值1,值2,值3)`;
+
+//  let a = esClient`/search/123123123?q=sdfsdf&cats=223123&basic=sdfsdfwer&ex_q_属性名=bterms(值1,值2,值3)`;
 
 export function esClient(string1) {
   console.log(string1);
 }
-// let a  =12312;
-
-// import styled from 'styled-components';
-// styled.button`1123`;
-
-let a = esClient`/search/123123123?q=sdfsdf&cats=223123&basic=sdfsdfwer`;
